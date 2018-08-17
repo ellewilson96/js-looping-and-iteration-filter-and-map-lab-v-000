82e94c0465cedaf906e34e31c2e1e8210424ff00
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-17 14:55:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ function driverNamesWithRevenueOver(drivers, revenue) {
        return driver.name;
      });
 }
+
+function exactMatch (drivers, matcher) {
+  return drivers.filter(function (driver) {
+    let matches = false;
+
+    for (const key in matcher) {
+      matches = driver[key] === matcher[key];
+    }
+
+    return matches;
+  });
+}
+
+function exactMatchToList (drivers, matcher) {
+  return exactMatch(drivers, matcher)
+    .map(function (driver) {
+      return driver.name;
+    });
+}
